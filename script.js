67bf6ad109e7b3ac56d0de1f2f02a71ee14b9eb7
yakinemejai <YAKINE.MEJAI@latoile.em-lyon.com>
2026-01-31 20:20:14 +0100

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+    
+  });
+});
